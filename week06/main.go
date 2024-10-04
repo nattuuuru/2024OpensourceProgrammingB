a0921032911dd5752e98b2342393da7e1a08c553
김나연 <natturu@gimnayeon-ui-MacBookPro.local>
2024-10-04 17:54:43 +0900

    v0.5 UNICODE

--- week06/main.go
+++ week06/main.go
@@ -8,10 +8,14 @@ import (
 func main() {
 	i := 13
 	var f float64 = 12.9 //f := 12.9
+	c1 := 'Z'
+	c2 := '김'
 
 	fmt.Printf("value i :%d, value f :%f\n", i, f)
 	//fmt.Print("%d * %f = %f\n", i, f, i*f) //invalid operation: i * f (mismatched types int and float64)
 	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) //conversion (casting)
-	//fmt.Printf("%d * %f = %f\n", i, f, i*int(f)) //conversion (casting)
-	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i))
+	//fmt.Printf("%d * %f = %f\n", i, f, i*int(f))     //conversion (casting)
+	fmt.Println(c1, c2)    // 10진수 출력
+	fmt.Printf("%x\n", c2) //유니코드 '김' 16진수 출력, UNICODE
+	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i), reflect.TypeOf(c1), reflect.TypeOf(c2))
 }
