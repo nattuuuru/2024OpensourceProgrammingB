db976fc88bcf08527886b99900c1173d99b1186c
김나연 <natturu@gimnayeon-ui-MacBookPro.local>
2024-10-11 18:45:59 +0900

    test2

--- week07/main.go
+++ week07/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	var float32 float32 = 9.1
-	fmt.Println((float32))
-	var number float32
+	fmt.Print("점수 입력 : ")
+	in := bufio.NewReader(os.Stdin)
+	score, err := in.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	score = strings.TrimSpace(score)                //줄바꿈, 띄어쓰기, 탭 등 제거 (Python strip과 유사)
+	realScore, _ := strconv.ParseInt(score, 10, 32) //정수형 32비트 타입으로 형변환, 10진수 입력
+
+	var grade string
+	if realScore >= 90 {
+		grade = "A"
+	} else {
+		grade = "BCDEF"
+
+	}
+	fmt.Printf("%d점은 %s등급입니다.\n", realScore, grade)
 }
