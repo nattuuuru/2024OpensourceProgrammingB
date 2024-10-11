731b20fa3b3283f7fbb6f60f12bf653d5ec5b2a4
김나연 <natturu@gimnayeon-ui-MacBookPro.local>
2024-10-11 17:23:53 +0900

    v1.0 if

--- week07/main.go
+++ week07/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -10,6 +11,10 @@ func main() {
 	fmt.Print("이름 입력 : ")
 	in := bufio.NewReader(os.Stdin)
 	name, err := in.ReadString('\n')
-	fmt.Println(name)
-	fmt.Println(err)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(name)
+	}
 }
