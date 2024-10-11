18de70046b8b1a5491984bb9885164f63fcd9686
김나연 <natturu@gimnayeon-ui-MacBookPro.local>
2024-10-11 16:57:48 +0900

    v0.8 Replace

--- week07/main.go
+++ week07/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"strings"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	//month := now.Month()
-	//fmt.Println(int(month))
-	fmt.Printf("오늘은 %d년 %d월 %d일입니다.\n", now.Year(), int(now.Month()), now.Day())
-	fmt.Printf("지금 시각은 %d시 %d분 %d초입니다.\n", now.Hour(), now.Minute(), now.Second())
+	var army string = "해군!"
+	armyFixed := strings.NewReplacer("!", "국")
+	fmt.Println(army)
+	fmt.Println(armyFixed.Replace(army))
 }
