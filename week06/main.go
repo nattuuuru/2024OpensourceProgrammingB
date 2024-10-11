af550c80a3cd93ef2b771e0d480475f3cc02c738
김나연 <natturu@gimnayeon-ui-MacBookPro.local>
2024-10-11 16:31:55 +0900

    v0.6 Zero value

--- week06/main.go
+++ week06/main.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	i := 13
-	var f float64 = 12.9 //f := 12.9
-	c1 := 'Z'
-	c2 := '김'
+	var f float64
+	var i int
+	var b bool
+	var s string
 
-	fmt.Printf("value i :%d, value f :%f\n", i, f)
-	//fmt.Print("%d * %f = %f\n", i, f, i*f) //invalid operation: i * f (mismatched types int and float64)
-	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) //conversion (casting)
-	//fmt.Printf("%d * %f = %f\n", i, f, i*int(f))     //conversion (casting)
-	fmt.Println(c1, c2)    // 10진수 출력
-	fmt.Printf("%x\n", c2) //유니코드 '김' 16진수 출력, UNICODE
-	fmt.Println(reflect.TypeOf(f), reflect.TypeOf(i), reflect.TypeOf(c1), reflect.TypeOf(c2))
+	fmt.Println(f, b, s, i)
+	fmt.Printf("%f%t%s%d\n", f, b, s, i)
+
+	i = 3
+	f = 2.7
+	fmt.Print("\n\n", f <= float64(i), "\n\n")
 }
